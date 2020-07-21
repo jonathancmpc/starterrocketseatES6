04106485fea60da0a93ed88fd5b27ef8ae962963
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-07-21 19:48:04 -0300

    Operaçoes com array

--- bundle.js
+++ bundle.js
@@ -69,4 +69,33 @@ var MinhaLista = new TodoList();
 
 document.getElementById('novotodo').onclick = function () {
   MinhaLista.add('Novo todo');
+  console.log(MinhaLista.usuario);
 };
+/* Operações com Array */
+
+
+var arr = [1, 3, 4, 5, 8, 10];
+/* Percorrendo com array e retornando a soma do item mais a sua posição no array */
+
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr);
+/* O reduce retorna o total e o valor subsequente no array, neste caso estamos somando tudo que tem no array */
+
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+/* Filtra tudo aquilo que é diferente de resto 0, ou seja, retorna números ímpares de dentro do array */
+
+var filter = arr.filter(function (item) {
+  return item % 2 !== 0;
+});
+console.log(filter);
+/* Procura dentro do array o número 10, se não estiver dentro do array retorna undefined */
+
+var find = arr.find(function (item) {
+  return item === 10;
+});
+console.log(find);
